bdb4df890c238c3f51d9833a6b583f9a2dff6835
valenja9 <kubavalen@seznam.cz>
2021-05-18 19:52:01 +0200

    Pridavam dalsi skript

---
+++ zkouska.py
@@ -0,0 +1,10 @@
+
+list_jmen = ["Jakub", "Petr", "Kamil", "Jarmila", "Josef"]
+
+
+for jmeno in list_jmen:
+    print("Toto vypisu pro kazde jmeno!")
+    if jmeno.startswith("K"):
+        print(jmeno)
+
+
